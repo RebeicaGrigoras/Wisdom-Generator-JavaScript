8b8836f98db80a1b3902186c76b680d5d029bf40
rebeicagrigoras <rebeicagrigoras25@gmail.com>
2025-01-05 12:43:48 +0000

    Added collectiveWisdom object with initial data for wisdom generation

--- main.js
+++ main.js
@@ -2,3 +2,16 @@
 function generateRandomNumber(num) {
   return Math.floor(Math.random() * num);
 }
+
+// An object containing arrays of different types of wisdom
+const collectiveWisdom = {
+  signInfo: ["star", "moon", "sun", "comet"],
+  fortuneOutput: [
+    "terrible luck",
+    "bad luck",
+    "ok luck",
+    "good luck",
+    "great luck",
+  ],
+  advice: ["go out and eat", "not read this", "play more", "trust no one"],
+};
